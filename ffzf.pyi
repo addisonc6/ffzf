3033d6e430de3825d4e1e8eca21329fcec42b540
Addison Cox <addisoncox@protonmail.com>
2022-03-16 17:09:13 -0400

    add closest index pair

--- ffzf.pyi
+++ ffzf.pyi
@@ -27,3 +27,17 @@ def n_closest(target: str, candidates: list[str], n: int, algorithm: str = "leve
     :param case_sensitive: Whether or not to use case sensitivity when finding the closest matches.
     """
     ...
+
+def closest_index_pair(target: str, text: str, algorithm: str = "levenshtein", case_sensitive: bool = False) -> tuple:
+    """
+    Find the the start and end index of the closest match to the target in the text.
+    :param target: The target string to find a match for.
+    :param text: The list of strings to find a match in.
+    :param algorithm: The algorithm to use for finding the closest match. Options are:
+        - "levenshtein"
+        - "jaro"
+        - "jarowinkler"
+        - "hamming"
+    :param case_sensitive: Whether or not to use case sensitivity when finding the closest match.
+    """
+    ...
