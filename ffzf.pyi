d2300d5f846ae3ab17201d963db81c110c9cb317
Addison Cox <addisoncox@protonmail.com>
2022-03-23 15:40:00 -0400

    add remove whitespace

--- ffzf.pyi
+++ ffzf.pyi
@@ -1,4 +1,9 @@
-def closest(target: str, candidates: list[str], algorithm: str = "levenshtein", case_sensitive: bool = False) -> str:
+def closest(
+    target: str, 
+    candidates: list[str], 
+    algorithm: str = "levenshtein", 
+    case_sensitive: bool = False, 
+    remove_whitespace: bool = False) -> str:
     """
     Find the closest match to the target string in the list of candidates.
     :param target: The target string to find a match for.
@@ -9,11 +14,17 @@ def closest(target: str, candidates: list[str], algorithm: str = "levenshtein", 
         - "jarowinkler"
         - "hamming"
     :param case_sensitive: Whether or not to use case sensitivity when finding the closest match.
+    :param remove_whitespace: Whether or not to remove whitespace when finding the closest match.
     """
     ...
 
 
-def n_closest(target: str, candidates: list[str], n: int, algorithm: str = "levenshtein", case_senstive: bool = False) -> list[str]:
+def n_closest(
+    target: str, 
+    candidates: list[str], 
+    n: int, algorithm: str = "levenshtein", 
+    case_senstive: bool = False, 
+    remove_whitespace: bool = False) -> list[str]:
     """
     Find the n closest matches to the target string in the list of candidates.
     :param target: The target string to find a match for.
@@ -25,10 +36,17 @@ def n_closest(target: str, candidates: list[str], n: int, algorithm: str = "leve
         - "jarowinkler"
         - "hamming"
     :param case_sensitive: Whether or not to use case sensitivity when finding the closest matches.
+    :param remove_whitespace: Whether or not to remove whitespace when finding the closest matches.
     """
     ...
 
-def closest_index_pair(target: str, text: str, algorithm: str = "levenshtein", case_sensitive: bool = False) -> tuple:
+
+def closest_index_pair(
+    target: str, 
+    text: str, 
+    algorithm: str = "levenshtein", 
+    case_sensitive: bool = False, 
+    remove_whitespace: bool = False) -> tuple[int, int]:
     """
     Find the the start and end index of the closest match to the target in the text.
     :param target: The target string to find a match for.
@@ -39,5 +57,6 @@ def closest_index_pair(target: str, text: str, algorithm: str = "levenshtein", c
         - "jarowinkler"
         - "hamming"
     :param case_sensitive: Whether or not to use case sensitivity when finding the closest match.
+    :param remove_whitespace: Whether or not to remove whitespace when finding the closest match.
     """
     ...
