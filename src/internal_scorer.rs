5841624d8712e9b5baf06c69b5820b594429d41c
Addison Cox <addisoncox@protonmail.com>
2022-04-07 19:40:01 -0400

    fn to reuse processed word for jw scorer

--- src/internal_scorer.rs
+++ src/internal_scorer.rs
@@ -93,6 +93,57 @@ pub fn jaro_similarity_target_preprocessed(
     Ok(jaro_similarity)
 }
 
+pub fn jaro_similarity_target_matched_preprocessed(
+    word1_chars: &Vec<char>,
+    word2_chars: &Vec<char>
+) -> PyResult<f32> {
+    if word1_chars == word2_chars {
+        return Ok(1.0);
+    }
+    let n = word1_chars.len();
+    let m = word2_chars.len();
+    let max_dist = (i32::max(m as i32, n as i32) / 2) - 1;
+    let mut matches = 0;
+    let mut hash_word1 = vec![0; n];
+    let mut hash_word2 = vec![0; m];
+    for i in 0..n {
+        let mut j = i32::max(i as i32 - max_dist, 0);
+        while j < i32::min(i as i32 + max_dist + 1, m as i32) {
+            if word1_chars[i] == word2_chars[j as usize] && hash_word2[j as usize] == 0 {
+                hash_word1[i] = 1;
+                hash_word2[j as usize] = 1;
+                matches += 1;
+                break;
+            }
+            j += 1;
+        }
+    }
+    if matches == 0 {
+        return Ok(0.0);
+    }
+    let mut transpositions = 0;
+    let mut point = 0;
+    for i in 0..n {
+        if hash_word1[i] != 0 {
+            while hash_word2[point] == 0 {
+                point += 1;
+            }
+            if word1_chars[i] != word2_chars[point] {
+                point += 1;
+                transpositions += 1;
+            } else {
+                point += 1;
+            }
+        }
+        transpositions /= 2;
+    }
+    let jaro_similarity = (matches as f32 / n as f32
+        + matches as f32 / m as f32
+        + (matches - transpositions) as f32 / matches as f32)
+        / 3.0;
+    Ok(jaro_similarity)
+}
+
 pub fn jaro_winkler_similarity_target_preprocessed(
     word1: &str,
     word2_chars: &Vec<char>,
@@ -105,10 +156,10 @@ pub fn jaro_winkler_similarity_target_preprocessed(
             "threshold must be between 0.0 and 1.0",
         ));
     }
-    let mut jaro_similarity =
-        jaro_similarity_target_preprocessed(word1, &word2_chars, case_sensitive, remove_whitespace, threshold)
-            .expect("Failed to calculate Jaro similarity.");
     let word1_chars = char_vec(word1, case_sensitive, remove_whitespace);
+    let mut jaro_similarity =
+        jaro_similarity_target_matched_preprocessed(&word1_chars, &word2_chars)
+            .expect("Failed to calculate Jaro similarity.");
     if jaro_similarity > threshold {
         let mut prefix = 0;
         for i in 0..usize::min(word1_chars.len(), word2_chars.len()) {
